a06fd33d59e604ea7d075ad876e0d44ab2d8da7e
ojas <ojasok@gmail.com>
2016-10-26 16:00:26 -0600

    proj3 commit1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+
+var storage= multer.diskStorage({
+    destination: function (req,file,cb) {
+        cb(null,'views/uploads/');
+    },
+
+    filename: function (req,file,cb) {
+        console.log(req);
+        cb(null,Date.now()+file.originalname);
+    }
+});
+
+var upload = multer({storage: storage});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,5 +36,61 @@ router.get('/About', function(req, res, next) {
   res.render('About', { title: 'Express' });
 });
 
+
+
+
+//for listing all files.
+router.get('/allFiles', function(req, res, next) {
+
+    const testFolder = 'views/uploads/';
+    const fs = require('fs');
+    var fi=[];
+    fs.readdir(testFolder, (err, files) => {
+
+        files.forEach(file => {
+            console.log(file);
+
+            fi.push(file);
+
+        })
+        console.log(fi);
+        res.render('allFiles',{ fila:fi});
+
+
+    })
+
+
+});
+
+
+///for downloading file
+router.get('/views/uploads/:file(*)', function(req, res, next){
+    var file = req.params.file;
+    var temp=__dirname;
+    var temp2=temp.substring(0,temp.length-7);
+    var path = temp2 + '/views/uploads/' + file;
+    //console.log(path);
+    res.download(path);
+});
+
+
+router.post('/apply',upload.any(),function(req, res, next) {
+    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    console.log(req.body.name);
+    console.log(req.body.lname);
+    console.log(req.body.tel);
+    console.log(req.body.email);
+    console.log(req.body.addr);
+    console.log(req.body.comment);
+    console.log(req.body.gridRadios);
+
+    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    //res.render(res);
+    res.render('apply');
+
+    res.send(req.files);
+
+
+});
 
 module.exports = router;
